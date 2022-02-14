7632cbb53cf48634409d8b5ff68f65866161960d
Yonghoon <yonghoonj98@gmail.com>
2022-02-14 11:44:30 +0900

    jest 테스트 코드 확인

---
+++ jest/jest.js
@@ -0,0 +1,5 @@
+function double(number) {
+  return number * 2;
+}
+
+module.exports = double;
---
+++ jest/test.js
@@ -0,0 +1,5 @@
+const double = require("./jest");
+
+test("2 * 2 to be 4", () => {
+  expect(double(2)).toBe(4);
+});
