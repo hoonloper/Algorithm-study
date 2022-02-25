d6f918dddb088a7725ee6f14deac34a785a831f6
Yonghoon <yonghoonj98@gmail.com>
2022-02-25 14:02:54 +0900

    js에서 interface 넣는 법

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "micle",
-    age: 22,
-    gender: "male",
-};
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const lynn = new Human("Lynn", 16, "female");
 const sayHi = (person) => {
     return `Hello ${person.name}, ${person.age}, ${person.gender}`;
 };
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,19 +1,20 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender?: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "micle",
-  age: 22,
-  gender: "male",
-};
+const lynn = new Human("Lynn", 16, "female");
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, ${person.age}, ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export {};
