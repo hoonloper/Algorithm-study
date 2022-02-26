7a071c369e21458034b6c4658cae90e19c1ac8c2
Yonghoon <yonghoonj98@gmail.com>
2022-02-26 15:44:10 +0900

    typescript 블록체인

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Human {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
-const lynn = new Human("Lynn", 16, "female");
-const sayHi = (person) => {
-    return `Hello ${person.name}, ${person.age}, ${person.gender}`;
-};
-console.log(sayHi(lynn));
+const genesisBlock = new Block(0, "202020202020", "", "hello", 123456);
+let blockchain = [genesisBlock];
+console.log(blockchain);
+/* 클래스 강의 내용 */
+// class Human {
+//   public name: string;
+//   public age: number;
+//   public gender: string;
+//   constructor(name: string, age: number, gender?: string) {
+//     this.name = name;
+//     this.age = age;
+//     this.gender = gender;
+//   }
+// }
+// const lynn = new Human("Lynn", 16, "female");
+// const sayHi = (person: Human): string => {
+//   return `Hello ${person.name}, ${person.age}, ${person.gender}`;
+// };
+// console.log(sayHi(lynn));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,20 +1,49 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender?: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const lynn = new Human("Lynn", 16, "female");
+const genesisBlock: Block = new Block(0, "202020202020", "", "hello", 123456);
 
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, ${person.age}, ${person.gender}`;
-};
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayHi(lynn));
+console.log(blockchain);
 
 export {};
+
+/* 클래스 강의 내용 */
+// class Human {
+//   public name: string;
+//   public age: number;
+//   public gender: string;
+//   constructor(name: string, age: number, gender?: string) {
+//     this.name = name;
+//     this.age = age;
+//     this.gender = gender;
+//   }
+// }
+
+// const lynn = new Human("Lynn", 16, "female");
+
+// const sayHi = (person: Human): string => {
+//   return `Hello ${person.name}, ${person.age}, ${person.gender}`;
+// };
+
+// console.log(sayHi(lynn));
