0f3ef3ca763a1c9fd763c9b82ad00cea52158dbf
Yonghoon <yonghoonj98@gmail.com>
2022-02-27 22:08:04 +0900

    백준 9 사칙연산 완료

---
+++ 백준/입출력과사칙연산/9.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+const val = input[0].split(" ");
+
+function solution(a, b) {
+  console.log(a + b);
+  console.log(a - b);
+  console.log(a * b);
+  console.log(Math.floor(a / b));
+  console.log(a % b);
+}
+
+solution(Number(val[0]), Number(val[1]));
