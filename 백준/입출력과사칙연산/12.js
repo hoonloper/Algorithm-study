35ba80f3ca24fe2124503dc73359656001859b1c
Yonghoon <yonghoonj98@gmail.com>
2022-02-27 22:23:58 +0900

    백준 12 완료

---
+++ 백준/입출력과사칙연산/12.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+const val = input[0].split(" ");
+
+function solution(A, B, C) {
+  console.log((A + B) % C);
+  console.log(((A % C) + (B % C)) % C);
+  console.log((A * B) % C);
+  console.log(((A % C) * (B % C)) % C);
+}
+
+solution(Number(val[0]), Number(val[1]), Number(val[2]));
