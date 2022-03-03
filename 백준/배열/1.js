7bd5467a103134d340b8d244313dfbb14fbca3bd
Yonghoon <yonghoonj98@gmail.com>
2022-03-03 14:06:16 +0900

    백준(배열) 1 완료

--- 백준/배열/1.js
+++ 백준/배열/1.js
@@ -1,6 +1,9 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = require("fs").readFileSync(filePath).toString();
+let input = require("fs").readFileSync(filePath).toString().split("\n");
 
-function solution(input) {}
+function solution(arr) {
+  arr = arr.map((item) => Number(item));
+  console.log(Math.min(...arr), Math.max(...arr));
+}
 
-solution(input);
+solution(input[1].split(" "));
