abe23548038636b8f25428a8eb08f8bfdff1a0ff
Yonghoon <yonghoonj98@gmail.com>
2022-03-04 23:34:21 +0900

    프로그래머스 소수찾기 완료

---
+++ 코딩테스트연습/프로그래머스/소수찾기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+  const arr = Array(n + 1)
+    .fill(true)
+    .fill(false, 0, 2);
+
+  for (let i = 2; i * i <= n; i++) {
+    if (arr[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        arr[j] = false;
+      }
+    }
+  }
+  console.log(arr);
+  return arr.filter((e) => e).length;
+}
