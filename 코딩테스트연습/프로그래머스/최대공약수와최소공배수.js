b537aebad6e85f2ec40351ecd090e59964c04744
Yonghoon <yonghoonj98@gmail.com>
2022-03-10 17:31:39 +0900

    프로그래머스 최대공약수와최소공배수 완료

---
+++ 코딩테스트연습/프로그래머스/최대공약수와최소공배수.js
@@ -0,0 +1,9 @@
+function solution(n, m) {
+  // 최대공약수, 유클리드 호제법... 어렵다..
+  const gcd = (n, m) => (n % m === 0 ? m : gcd(m, n % m));
+
+  // 최소공배수, n과 m 곱한값에 최대공약수로 나누어줌
+  const lcm = (n, m) => (n * m) / gcd(n, m);
+
+  return [gcd(n, m), lcm(n, m)];
+}
