d84dd45251e37e1576ff286d9240a67a4512104d
Yonghoon <yonghoonj98@gmail.com>
2022-12-10 15:53:32 +0900

    Add: 분수의 덧셈 코드 추가

---
+++ 코딩테스트연습/프로그래머스/분수의덧셈.js
@@ -0,0 +1,13 @@
+function solution(denum1, num1, denum2, num2) {
+  let denum = denum1 * num2 + denum2 * num1;
+  let num = num1 * num2;
+  let max = 1;
+
+  for (let i = 1; i <= denum; i++) {
+    if (denum % i === 0 && num % i === 0) {
+      max = i;
+    }
+  }
+
+  return [denum / max, num / max];
+}
