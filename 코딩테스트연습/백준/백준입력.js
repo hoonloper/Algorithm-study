4912f12e35da963fb39f6570a8481d1b1c864e6e
Yonghoon <yonghoonj98@gmail.com>
2022-12-31 09:25:34 +0900

    Update: 백준 입력 코드 수정

--- 코딩테스트연습/백준/백준입력.js
+++ 코딩테스트연습/백준/백준입력.js
@@ -1,13 +1,12 @@
-const readline = require('readline');
-const rl = readline.createInterface({
+const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
 const input = [];
 
-rl.on('line', function (line) {
-  input.push(line);
-}).on('close', function () {
-  // 풀이
-});
+readline
+  .on('line', (line) => input.push(line))
+  .on('close', () => {
+    // 풀이
+  });
