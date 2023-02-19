7cf54ea33e94038be6ac1688ec8c44d3cdb94e6a
Yonghoon <yonghoonj98@gmail.com>
2023-02-19 20:18:37 +0900

    Add: 프로그래머스 2단계 N개의 최소공배수 코드 추가

---
+++ 프로그래머스/level-2/level2-N개의최소공배수.js
@@ -0,0 +1,8 @@
+function solution(arr) {
+  return arr.reduce((acc, cur) => {
+    const recursive = (min, max) => {
+      return min % max === 0 ? max : recursive(max, min % max);
+    };
+    return (acc * cur) / recursive(acc, cur);
+  });
+}
