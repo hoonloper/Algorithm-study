6b7283551f91a991d28f37b1ed38f358f7ebb5b2
Yonghoon <yonghoonj98@gmail.com>
2023-03-27 21:21:10 +0900

    Add: 프로그래머스 레벨2 행렬의 곱셈 코드 추가

---
+++ 프로그래머스/level-2/level2-행렬의곱셈.js
@@ -0,0 +1,27 @@
+function solution(arr1, arr2) {
+  return arr1.map((arr, i) =>
+    arr2[0].map((_, j) =>
+      arr.reduce((sum, num, z) => sum + num * arr2[z][j], 0)
+    )
+  );
+}
+
+/*
+function solution(arr1, arr2) {
+    const answer = []
+
+    for(let i = 0; i < arr1.length; i++) {
+        const arr = [];
+        for(let j = 0; j < arr2[0].length; j++) {
+            let elem = 0;
+            
+            for(let z = 0; z < arr1[0].length; z++) {
+                elem += arr1[i][z] * arr2[z][j]
+            }
+            arr.push(elem)
+        }
+        answer.push(arr)
+    }
+    return answer;
+}
+*/
