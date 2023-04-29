4f119ed7171cdd330a7ea079fb6490b7e0acb3d2
hoonloper <yonghoonj98@gmail.com>
2023-04-29 15:26:36 +0900

    Add: 프로그래머스 레벨 0 날짜 비교하기

---
+++ JavaScript/프로그래머스/level-0/level0-날짜비교하기.js
@@ -0,0 +1,13 @@
+function solution(date1, date2) {
+  return date1[0] > date2[0]
+    ? 0
+    : date1[0] < date2[0]
+    ? 1
+    : date1[1] > date2[1]
+    ? 0
+    : date1[1] < date2[1]
+    ? 1
+    : date1[2] >= date2[2]
+    ? 0
+    : 1;
+}
