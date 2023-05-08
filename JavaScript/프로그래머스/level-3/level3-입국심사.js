cd3c52ff3d87963a863b88c8cf2de00ab977779d
hoonloper <yonghoonj98@gmail.com>
2023-05-08 22:31:41 +0900

    Refactor: 프로그래머스 레벨3 입국심사

--- JavaScript/프로그래머스/level-3/level3-입국심사.js
+++ JavaScript/프로그래머스/level-3/level3-입국심사.js
@@ -5,7 +5,11 @@ function solution(n, times) {
   while (lt <= rt) {
     const mid = Math.floor((lt + rt) / 2);
     const sum = times.reduce((acc, time) => acc + Math.floor(mid / time), 0);
-    sum < n ? (lt = mid + 1) : (rt = mid - 1);
+    if (sum < n) {
+      lt = mid + 1;
+    } else {
+      rt = mid - 1;
+    }
   }
 
   return lt;
