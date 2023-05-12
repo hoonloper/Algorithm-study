5129ce1bca632abbb43c132fa97fbca206dac2c1
hoonloper <yonghoonj98@gmail.com>
2023-05-12 21:55:06 +0900

    Add: 백준 이분탐색 3079

--- JavaScript/백준/알고리즘분류/이분탐색/3079-입국심사.js
+++ JavaScript/백준/알고리즘분류/이분탐색/3079-입국심사.js
@@ -9,21 +9,21 @@ readline
   .on('line', (line) => input.push(line))
   .on('close', () => {
     const [n, m] = input.shift().split(' ').map(Number);
-    const arr = input.map(Number).sort((a, b) => a - b);
+    const arr = input.map(BigInt);
 
-    let s = 0;
-    let e = m * arr[arr.length - 1];
+    let s = 0n;
+    let e = BigInt(m) * arr[arr.length - 1];
 
     while (s <= e) {
-      const mid = Math.floor((s + e) / 2);
+      const mid = BigInt((s + e) / 2n);
 
-      const p = arr.reduce((acc, cur) => acc + Math.floor(mid / cur), 0);
+      const p = arr.reduce((acc, cur) => acc + BigInt(mid / cur), 0n);
 
       if (p >= m) {
-        e = mid - 1;
+        e = mid - 1n;
       } else {
-        s = mid + 1;
+        s = mid + 1n;
       }
     }
-    console.log(s);
+    console.log(s.toString());
   });
