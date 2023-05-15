a0e7a550a5b97e33d66a0c1e50b88586fef294f9
hoonloper <yonghoonj98@gmail.com>
2023-05-15 20:41:05 +0900

    Add: 프로그래머스 레벨0 배열 만들기  5

---
+++ JavaScript/프로그래머스/level-0/level0-배열만들기5.js
@@ -0,0 +1,8 @@
+function solution(intStrs, k, s, l) {
+  return intStrs.reduce((acc, cur) => {
+    if (+cur.slice(s, s + l) > k) {
+      acc.push(+cur.slice(s, s + l));
+    }
+    return acc;
+  }, []);
+}
