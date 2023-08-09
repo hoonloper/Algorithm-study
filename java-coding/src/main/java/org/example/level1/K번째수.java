73f68b2c421492eea3b42981bd9bea999529dc24
hoonloper <yonghoonj98@gmail.com>
2023-08-09 21:15:08 +0900

    Add: 프로그래머스 레벨 1 K번째 수

--- java-coding/src/main/java/org/example/level1/K번째수.java
+++ java-coding/src/main/java/org/example/level1/K번째수.java
@@ -1,8 +1,8 @@
-package org.example;
+package org.example.level1;
 
 import java.util.Arrays;
 
-public class Main {
+public class K번째수 {
     public static void main(String[] args) {
         int[] array = { 1, 5, 2, 6, 3, 7, 4 };
         int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
---
+++ java-coding/src/main/java/org/example/level1/크레인인형뽑기.java
@@ -0,0 +1,4 @@
+package org.example.level1;
+
+public class 크레인인형뽑기 {
+}
