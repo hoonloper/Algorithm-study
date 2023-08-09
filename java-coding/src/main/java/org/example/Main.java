68add62732dfe4d3c6d290ff11c21d5631023869
hoonloper <yonghoonj98@gmail.com>
2023-08-09 21:14:00 +0900

    Setting: Java Coding Test project

---
+++ java-coding/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = { 1, 5, 2, 6, 3, 7, 4 };
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+
+        int len = commands.length;
+        int[] answer = new int[len];
+
+        for(int i = 0; i < len; i++) {
+            int[] newArr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(newArr);
+            System.out.println(Arrays.toString(newArr) + commands[i][0] + commands[i][1] +commands[i][2]);
+            answer[i] = newArr[commands[i][2] - 1];
+        }
+
+        System.out.println(Arrays.toString(answer));
+    }
+}
