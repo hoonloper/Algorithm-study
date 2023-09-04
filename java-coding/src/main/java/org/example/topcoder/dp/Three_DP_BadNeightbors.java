ab20e040caf688ba8a79957c8d23f20ff6f80099
hoonloper <yonghoonj98@gmail.com>
2023-09-04 20:31:28 +0900

    Add: 탑코더 DP 나쁜 이웃 코드 추가

---
+++ java-coding/src/main/java/org/example/topcoder/dp/Three_DP_BadNeightbors.java
@@ -0,0 +1,56 @@
+package org.example.topcoder.dp;
+public class Three_DP_BadNeightbors {
+    public static void main(String[] args) {
+//        int[] donations = {10, 3, 2, 5, 7, 8};
+//        int[] donations = {11, 15};
+        int[] donations = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+        maxDonation(donations);
+    }
+
+    private static void maxDonation(int[] donations) {
+        int i;
+        int ans = 0;
+
+        int N = donations.length;
+        int[] dp = new int[N];
+
+        for(i = 0; i < N - 1; i++) {
+            dp[i] = donations[i];
+            if(i > 0) dp[i] = Math.max(dp[i], dp[i - 1]);
+            if(i > 1) dp[i] = Math.max(dp[i], dp[i - 2] + donations[i]);
+            ans = Math.max(ans, dp[i]);
+        }
+
+        for(i = 0; i < N - 1; i++) {
+            dp[i] = donations[i + 1];
+            if(i > 0) dp[i] = Math.max(dp[i], dp[i - 1]);
+            if(i > 1) dp[i] = Math.max(dp[i], dp[i - 2] + donations[i + 1]);
+            ans = Math.max(ans, dp[i]);
+        }
+
+        System.out.println(ans);
+
+        // 초기 풀이
+//        int[] arr = new int[donations.length * 2];
+//        for(int i = 0; i < arr.length; i++) {
+//            if(i >= donations.length) {
+//                arr[i] = donations[i - donations.length];
+//            } else {
+//                arr[i] = donations[i];
+//            }
+//        }
+//
+//        int max = 0;
+//        for(int i = 0; i <donations.length; i++) {
+//            int sum = 0;
+//            for(int j = i; j < i + donations.length; j += 2) {
+//                System.out.println(j + " " + arr[j]);
+//                sum += arr[j];
+//            }
+//            System.out.println();
+//            max = Math.max(max, sum);
+//        }
+//        System.out.println(max);
+
+    }
+}
