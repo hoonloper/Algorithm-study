533e78120b6327690d09093523a4cfb331524d47
hoonloper <yonghoonj98@gmail.com>
2023-09-05 20:33:34 +0900

    Add: DP 가방 문제 복습 코드 추가2

--- java-coding/src/main/java/org/example/topcoder/dp/One_DP_Knapsack.java
+++ java-coding/src/main/java/org/example/topcoder/dp/One_DP_Knapsack.java
@@ -1,15 +1,32 @@
 package org.example.topcoder.dp;
 
-import java.util.Arrays;
-
 public class One_DP_Knapsack {
     public static void main(String[] args) {
         int[] ws = {3,4,1,2,3};
         int[] ps = {2,3,2,3,6};
         int maxw = 10;
-        dp(ws,ps,maxw);
-        customDp(ws, ps, maxw);
+//        dp(ws,ps,maxw);
+//        customDp(ws, ps, maxw);
+        customDp2(ws, ps, maxw);
     }
+
+    private static void customDp2(int[] ws, int[] ps, int maxw) {
+        int ans = 0;
+        int[][] dp = new int[ws.length + 1][maxw + 1];
+
+        for(int i = 0; i < ws.length; i++) {
+            for(int j = 0; j <= maxw; j++) {
+                if(j + ws[i] <= maxw) {
+                    dp[i + 1][j + ws[i]] = Math.max(dp[i + 1][j + ws[i]], dp[i][j] + ps[i]);
+                    ans = Math.max(ans, dp[i+1][j+ws[i]]);
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+
+
+
     private static void customDp(int[] ws, int[] ps, int maxw) {
         int ans = 0;
         int[][] dp = new int[ws.length + 1][maxw + 1];
