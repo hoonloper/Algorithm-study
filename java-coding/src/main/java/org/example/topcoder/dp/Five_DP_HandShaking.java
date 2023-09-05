b61d8f0b997ed19e1d422061c68cb2d70f045f91
hoonloper <yonghoonj98@gmail.com>
2023-09-05 19:52:39 +0900

    Add: 탑코더 DP 악수 코드 추가

---
+++ java-coding/src/main/java/org/example/topcoder/dp/Five_DP_HandShaking.java
@@ -0,0 +1,22 @@
+package org.example.topcoder.dp;
+
+public class Five_DP_HandShaking {
+    public static void main(String[] args) {
+        long answer = countPerfect(2);
+        System.out.println(answer);
+    }
+
+    private static long countPerfect(int n) {
+        long[] dp = new long[n / 2 + 1];
+        dp[0] = 1;
+
+        for(int i = 1; i <= n / 2; i++) {
+            dp[i] = 0;
+            for(int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+
+        return dp[n / 2];
+    }
+}
