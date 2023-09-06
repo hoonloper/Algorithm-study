fc83a082588ac5f494b889fc83f39eae8f0d08c1
hoonloper <yonghoonj98@gmail.com>
2023-09-06 19:55:55 +0900

    Add: 백준 DP 9655 코드추가

---
+++ java-coding/src/main/java/org/example/boj/S5_9655.java
@@ -0,0 +1,17 @@
+package org.example.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S5_9655 {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            int N = Integer.parseInt(br.readLine());
+
+            System.out.println(N % 2 == 1 ? "SK" : "CY");
+            br.close();
+        }
+
+}
