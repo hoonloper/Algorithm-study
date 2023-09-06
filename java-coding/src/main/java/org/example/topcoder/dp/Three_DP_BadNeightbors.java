29a428e1948e55b0f1203c3d5938fada99d1d481
hoonloper <yonghoonj98@gmail.com>
2023-09-06 18:57:11 +0900

    Add: DP 나쁜 이웃 복습 코드 추가

--- java-coding/src/main/java/org/example/topcoder/dp/Three_DP_BadNeightbors.java
+++ java-coding/src/main/java/org/example/topcoder/dp/Three_DP_BadNeightbors.java
@@ -4,8 +4,32 @@ public class Three_DP_BadNeightbors {
 //        int[] donations = {10, 3, 2, 5, 7, 8};
 //        int[] donations = {11, 15};
         int[] donations = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
-        maxDonation(donations);
+//        maxDonation(donations);
+        customMaxDonation(donations);
     }
+
+    private static void customMaxDonation(int[] donations) {
+        int size = donations.length;
+        int[] dp = new int[size];
+        int ans = 0;
+
+        for(int i = 0; i < size - 1; i++) {
+            dp[i] = donations[i];
+            if(i > 0) dp[i] = Math.max(dp[i], dp[i - 1]);
+            if(i > 1) dp[i] = Math.max(dp[i], dp[i - 2] + donations[i]);
+            ans = Math.max(ans, dp[i]);
+        }
+
+        for(int i = 0; i < size - 1; i++) {
+            dp[i] = donations[i + 1];
+            if(i > 0) dp[i] = Math.max(dp[i], dp[i - 1]);
+            if(i > 1) dp[i] = Math.max(dp[i], dp[i - 2] + donations[i]);
+            ans = Math.max(ans, dp[i]);
+        }
+
+        System.out.println(ans);
+    }
+
 
     private static void maxDonation(int[] donations) {
         int i;
