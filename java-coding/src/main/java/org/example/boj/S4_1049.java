a8db8f3c06da54e5cedb90db1276fdfe4a0ccb94
hoonloper <yonghoonj98@gmail.com>
2023-09-09 22:14:15 +0900

    Add: 백준 S4_1049 코드 추가

---
+++ java-coding/src/main/java/org/example/boj/S4_1049.java
@@ -0,0 +1,32 @@
+package org.example.boj;
+
+
+import java.io.*;
+import java.util.*;
+public class S4_1049 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] mn = br.readLine().split(" ");
+        int n = Integer.parseInt(mn[0]);
+        int m = Integer.parseInt(mn[1]);
+
+        int[] packages = new int[m];
+        int[] units = new int[m];
+        for(int i = 0; i < m; i++) {
+            String[] str = br.readLine().split(" ");
+            packages[i] = Integer.parseInt(str[0]);
+            units[i] = Integer.parseInt(str[1]);
+        }
+
+        Arrays.sort(packages);
+        Arrays.sort(units);
+
+        int min;
+        //가장 싼 6개짜리 팩 구매 vs 가장 싼 낱개 구매 vs (가장싼 6개짜리 팩 + 낱개)
+        min = Math.min(((n/6)+1)*packages[0], n*units[0]);
+        min = Math.min(min, ((n/6))*packages[0] + (n%6)*units[0]);
+
+        System.out.println(min);
+    }
+}
