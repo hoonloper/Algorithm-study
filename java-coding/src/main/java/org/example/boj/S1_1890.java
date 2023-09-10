5b1bdc5726d8f2144f7017c7be95962b63ea608b
hoonloper <yonghoonj98@gmail.com>
2023-09-10 15:49:18 +0900

    Add: 백준 S1_1890 DP 코드 추가

---
+++ java-coding/src/main/java/org/example/boj/S1_1890.java
@@ -0,0 +1,44 @@
+package org.example.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S1_1890 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String[][] arr = new String[n][];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = br.readLine().split(" ");
+        }
+
+        long[][] board = new long[n][n];
+
+        board[0][0] = 1;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(Long.parseLong(arr[i][j]) == 0) {
+                    break;
+                }
+                if(board[i][j] > 0) {
+                    long jump = Long.parseLong(arr[i][j]);
+                    int b = (int) (i + jump);
+                    int r = (int) (j + jump);
+                    boolean bJump = (b) < n;
+                    boolean rJump = (r) < n;
+                    if(bJump) {
+                        board[b][j] += board[i][j];
+                    }
+                    if(rJump) {
+                        board[i][r] += board[i][j];
+                    }
+                }
+            }
+        }
+        System.out.println(board[n - 1][n - 1]);
+    }
+}
