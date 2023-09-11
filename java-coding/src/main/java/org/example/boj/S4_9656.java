fcc6de00854c68596fee9ea980bb18b3dcbcb1f8
hoonloper <yonghoonj98@gmail.com>
2023-09-11 20:38:24 +0900

    Add: 백준 S4_9656 DP 코드 추가

--- java-coding/src/main/java/org/example/boj/S4_9656.java
+++ java-coding/src/main/java/org/example/boj/S4_9656.java
@@ -2,10 +2,11 @@ package org.example.boj;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class S4_9656 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
