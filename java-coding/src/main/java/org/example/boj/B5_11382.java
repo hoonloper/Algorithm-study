1cf6c8424c3b9b1ac7fac776ccf33291b4da8f37
hoonloper <yonghoonj98@gmail.com>
2023-09-18 20:41:24 +0900

    Add: 백준 B5_11382 코드 추가

---
+++ java-coding/src/main/java/org/example/boj/B5_11382.java
@@ -0,0 +1,18 @@
+package org.example.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B5_11382 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long ans = 0;
+        while (st.hasMoreTokens()) {
+            ans += Long.parseLong(st.nextToken());
+        }
+        System.out.println(ans);
+    }
+}
