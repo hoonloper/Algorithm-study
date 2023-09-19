836b6ab94541aea853cad4618fffa8f304de372b
hoonloper <yonghoonj98@gmail.com>
2023-09-19 20:06:15 +0900

    Add: 백준 B5_3003 코드 추가

---
+++ java-coding/src/main/java/org/example/boj/B5_3003.java
@@ -0,0 +1,19 @@
+package org.example.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B5_3003 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arr = br.readLine().split(" ");
+        int[] origin = new int[]{1, 1, 2, 2, 2, 8};
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < 6; i++) {
+            sb.append(origin[i] - Integer.parseInt(arr[i])).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+}
