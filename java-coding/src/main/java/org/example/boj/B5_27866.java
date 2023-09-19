106427c1f890831f60c8ef4685986456c52cac11
hoonloper <yonghoonj98@gmail.com>
2023-09-19 19:39:18 +0900

    Add: 백준 B5_27866 코드 추가

---
+++ java-coding/src/main/java/org/example/boj/B5_27866.java
@@ -0,0 +1,13 @@
+package org.example.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B5_27866 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int index = Integer.parseInt(br.readLine());
+        System.out.println(str.charAt(index-1));
+    }
+}
