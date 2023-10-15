e30034fd75ccd392d09601301694761264faa362
hoonloper <yonghoonj98@gmail.com>
2023-10-15 17:06:26 +0900

    Add: BOJ B3_2566

---
+++ java-coding/src/main/java/org/example/boj/B3_2566.java
@@ -0,0 +1,28 @@
+package org.example.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B3_2566 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int max = 0;
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i < 9; i++) {
+            String[] nums = br.readLine().split(" ");
+            for(int j = 0; j < 9; j++) {
+                int num = Integer.parseInt(nums[j]);
+                if(num > max) {
+                    max = num;
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println((x + 1) + " " + (y + 1));
+    }
+}
