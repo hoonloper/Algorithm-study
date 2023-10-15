8fe336bae38733f940044bb5c0a234720cb52e12
hoonloper <yonghoonj98@gmail.com>
2023-10-15 17:26:13 +0900

    Add: B5_24262

---
+++ java-coding/src/main/java/org/example/boj/B5_24262.java
@@ -0,0 +1,8 @@
+package org.example.boj;
+
+public class B5_24262 {
+    public static void main(String[] args) throws Exception {
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
