98478ed50532de282575126d5bf50e87a79c516d
hoonloper <yonghoonj98@gmail.com>
2023-10-15 17:28:23 +0900

    Add: B4_24263

---
+++ java-coding/src/main/java/org/example/boj/B4_24263.java
@@ -0,0 +1,12 @@
+package org.example.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B4_24263 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine());
+        System.out.println(1);
+    }
+}
