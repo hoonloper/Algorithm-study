acc8da84a62af7bb0fc80fac78e979f69291a769
hoonloper <yonghoonj98@gmail.com>
2023-10-15 17:31:28 +0900

    Add: B3_24264

---
+++ java-coding/src/main/java/org/example/boj/B3_24264.java
@@ -0,0 +1,13 @@
+package org.example.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B3_24264 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Long n = Long.parseLong(br.readLine());
+        System.out.println(n * n);
+        System.out.println(2);
+    }
+}
