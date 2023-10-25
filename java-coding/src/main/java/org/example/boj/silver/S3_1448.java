998a55ed0b71bc0f0e35ca4a6a6f605cd83ff105
hoonloper <yonghoonj98@gmail.com>
2023-10-25 19:04:38 +0900

    Add: S4_1448

---
+++ java-coding/src/main/java/org/example/boj/silver/S3_1448.java
@@ -0,0 +1,33 @@
+package org.example.boj.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class S3_1448 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+
+        List<Integer> arr = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            arr.add(Integer.parseInt(br.readLine()));
+        }
+
+        arr.sort(Collections.reverseOrder());
+
+        int max = 0;
+        for (int i = 0; i < n - 2; i++) {
+            if(arr.get(i) < arr.get(i + 1) + arr.get(i + 2)) {
+                max = Math.max(max, arr.get(i) + arr.get(i + 1) + arr.get(i + 2));
+            }
+        }
+        System.out.println(max == 0 ? -1 : max);
+    }
+}
