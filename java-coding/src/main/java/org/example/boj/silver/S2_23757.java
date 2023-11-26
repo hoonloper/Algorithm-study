8629e4f365664f009bb819d35407331ed2d19ad6
hoonloper <yonghoonj98@gmail.com>
2023-11-26 14:00:46 +0900

    Add: S2_23757

---
+++ java-coding/src/main/java/org/example/boj/silver/S2_23757.java
@@ -0,0 +1,34 @@
+package org.example.boj.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class S2_23757 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            pq.add(Integer.parseInt(st.nextToken()));
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int cur = Integer.parseInt(st.nextToken());
+            int max = pq.poll();
+            if (cur > max) {
+                System.out.println(0);
+                return;
+            }
+            pq.add(max-cur);
+        }
+        System.out.println(1);
+    }
+}
